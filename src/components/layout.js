96664e41703eee693916103f21ae69c20530dcb8
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-01-29 01:58:57 +0900

    【追加】レイアウトにブログページへのリンク追加

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,6 +36,11 @@ export const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to='/blog' className={navLinkText}>
+              Blog
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
