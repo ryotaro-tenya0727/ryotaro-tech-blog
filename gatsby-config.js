9428df6f9b41f163eb7423ca04974b6b913a56ab
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-01-28 19:44:09 +0900

    【追加】画像を表示するプラグインの追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `Ryotaro Tech Blog`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: `Ryotaro Tech Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+};
